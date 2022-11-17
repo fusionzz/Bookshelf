630156c0b7e70bbb35b39d58ca4ff3ca454dfeb7
David <davidveller@gmail.com>
2022-11-16 21:00:24 -0500

    added sorting by title or author

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -27,12 +27,12 @@ export class BooksComponent implements OnInit {
   currBooks?:any[] = [];
   currPage:number = 0;
 
-  totalItems = 0;
-
+  //creates empty array of size i
   toRange(i:number){
     return new Array(i);
   }
 
+  //loads first page so we arent waiting for nothing
   getFirstPage(){
     this.bookService.getHomeLibrary(0).subscribe(
       response => {
@@ -46,6 +46,7 @@ export class BooksComponent implements OnInit {
     return lastValueFrom(this.bookService.getHomeLibrary(index));
   }
 
+  //gets all the books to populate pages
   async getAllBooks(){
     let currBooks:any[] = [];
     let i = 1;
@@ -58,15 +59,66 @@ export class BooksComponent implements OnInit {
       this.numPages++;
       i++;         
     } while (currBooks.length == this.itemsPerPage)
-
-
   }
 
 
   //for changing pages
   setBooks(page:number){
     this.currBooks = this.allBooks?.slice(page*this.itemsPerPage, page*this.itemsPerPage+this.itemsPerPage);
-    //console.log(this.allBooks)
+  }
+
+  //sort by title
+  titleAsc:boolean = false;
+  titleSorted:boolean = false;
+
+  titleSort(){
+    this.authorSorted = false;
+    this.authorAsc = false;
+    this.titleSorted = true;
+    this.titleAsc = !this.titleAsc;
+    this.allBooks?.sort((a,b) => {
+      if (this.titleAsc){
+        if (a.volumeInfo.title < b.volumeInfo.title){
+          return -1;
+        }else{
+          return 1;
+        }
+      }else{
+        if (a.volumeInfo.title < b.volumeInfo.title){
+          return 1;
+        }else{
+          return -1;
+        }
+      }
+    })
+    this.setBooks(0);
+  }
+
+  //sort by author first name
+  authorAsc:boolean = false;
+  authorSorted:boolean = false;
+
+  authorSort(){
+    this.titleSorted = false;
+    this.titleAsc = false;
+    this.authorSorted = true;
+    this.authorAsc = !this.authorAsc;
+    this.allBooks?.sort((a,b) => {
+      if (this.authorAsc){
+        if (a.volumeInfo.authors < b.volumeInfo.authors){
+          return -1;
+        }else{
+          return 1;
+        }
+      }else{
+        if (a.volumeInfo.authors < b.volumeInfo.authors){
+          return 1;
+        }else{
+          return -1;
+        }
+      }
+    })
+    this.setBooks(0);
   }
 
 }
